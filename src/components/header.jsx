eb835399a409c17f6e4458b6b4bc518ee687a371
dexterR35 <pusaw202@gmail.com>
2023-10-27 17:23:59 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa6";
+
+const Header = () => {
+  let Links = [
+    { name: "Acasa", link: "/" },
+    { name: "Despre Noi", link: "/" },
+    { name: "Oferta", link: "/" },
+    { name: "Contact", link: "/" },
+  ];
+
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+    <nav className="w-full bg-green-100 shadow">
+      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <a href="javascript:void(0)">
+              <h2 className="text-2xl font-bold text-white">LOGO</h2>
+            </a>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-white"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6 text-white"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+              {Links.map((link) => (
+                <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+                  <a
+                    href={link.link}
+                    className="text-gray-800 hover:text-gray-400 duration-500"
+                  >
+                    {link.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+
+            <div className="mt-3 space-y-2 lg:hidden md:hidden">
+              <a
+                href="javascript:void(0)"
+                className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+              >
+                Vezi Oferta
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="hidden space-x-2 md:inline-block">
+          <a
+            href="javascript:void(0)"
+            className="px-4 py-2 text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+          >
+            Vezi Oferta
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+        green: {
+          100: "#D0E7D2",
+          200: "#B0D9B1",
+          300: "#79AC78",
+          400: "#618264",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
