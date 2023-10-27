40af274c5884c9d635245b93889d19c65c626699
dexterR35 <pusaw202@gmail.com>
2023-10-27 18:20:21 +0300

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -21,38 +21,10 @@ const Header = () => {
             </a>
             <div className="md:hidden">
               <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                className="p-2 text-gray-700 text-2xl rounded-md outline-none focus:border-gray-400 focus:border"
                 onClick={() => setNavbar(!navbar)}
               >
-                {navbar ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
+                {navbar ? <FaBars /> : <FaBars />}
               </button>
             </div>
           </div>
@@ -63,9 +35,9 @@ const Header = () => {
               navbar ? "block" : "hidden"
             }`}
           >
-            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            <ul className="items-center justify-center space-y-5 md:flex md:space-x-6 md:space-y-0">
               {Links.map((link) => (
-                <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+                <li key={link.name} className="md:ml-8 text-lg md:my-0 my-4">
                   <a
                     href={link.link}
                     className="text-gray-800 hover:text-gray-400 duration-500"
@@ -79,7 +51,7 @@ const Header = () => {
             <div className="mt-3 space-y-2 lg:hidden md:hidden">
               <button
                 href="javascript:void(0)"
-                className="inline-block w-full px-4 py-2 text-center text-gray-800 bg-white rounded-md shadow hover:bg-gray-100"
+                className="inline-block w-full px-4 py-2 text-center text-white bg-green-400 rounded-md shadow hover:bg-gray-100"
               >
                 Vezi Oferta
               </button>
