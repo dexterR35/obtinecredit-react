4a4ed7872d95ac2bd0162ec5c9820cfd13a88766
dexterR35 <pusaw202@gmail.com>
2023-11-01 12:15:45 +0200

    update aboutUs

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,27 +1,57 @@
 const about = () => {
   return (
-    <section className="container mx-auto md:h-screen h-100 bg-green-100 flex flex-col-reverse md:flex-row items-center">
-      <div className="w-96 md:w-1/2">
-        <img src={require("../assets/person1.png")} alt="person1" />
-      </div>
-      <div className="w-96 md:w-1/2">
-        <h2>Cine suntem?</h2>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut soluta
-          itaque laboriosam officiis odit. Tempore saepe enim nemo. Voluptas,
-          modi Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Distinctio inventore architecto explicabo enim repudiandae minima quis
-          qui voluptatibus accusamus minus.
-        </p>
-        <h4>Ce putem face!</h4>
-        <ul>
-          <li>putem face asta</li>
-          <li>putem face asta</li>
-          <li>putem face asta</li>
-          <li>putem face asta</li>
-          <li>putem face asta</li>
-        </ul>
-        <button type="button">Aplica aici</button>
+    <section className="bg-green-100 ">
+      <div className="container mx-auto h-100 lg:h-screen flex flex-col items-center justify-evenly py-4 lg:py-0">
+        <h2 className="text-4xl font-bold uppercase flex flex-col text-center mt-[30px] mb-[20px] lg:py-0">
+          Cine suntem?
+          <span className="text-sm capitalize font-normal text-gray-700">
+            Lorem ipsum dolor sit.
+          </span>
+        </h2>
+        <div className="h-auto lg:h-28 bg-green-00 text-center my-[20px] lg:hidden block">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+            nobis labore, fugiat a excepturi, cum ducimus eos laborum ut quos
+            facere possimus perspiciatis odit, magnam laboriosam velit beatae
+            architecto neque?
+          </p>
+        </div>
+        <div className="flex flex-col-reverse lg:flex-row items-center p-4">
+          <div className="w-96 md:w-1/2">
+            <img
+              src={require("../assets/person1.png")}
+              alt="person1"
+              className="lg:w-96 lg:block w-48 mx-auto hidden"
+            />
+          </div>
+          <div className="w-96 md:w-1/2 bg-white flex items-start flex-col p-5 space-y-3 rounded-[15px] shadow-lg">
+            <h3 className="text-xl font-bold">Echipa Racheta</h3>
+            <p className="w-3/3">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut
+              soluta itaque laboriosam officiis odit. Tempore saepe enim nemo.
+              Voluptas, modi Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Distinctio inventore architecto explicabo enim repudiandae
+              minima quis qui voluptatibus accusamus minus.
+            </p>
+            <h3 className="text-xl font-bold">De ce Noi!?</h3>
+            <ul>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit amet.</li>
+              <li>Lorem ipsum dolor sit amet consectetur.</li>
+              <li>Lorem ipsum dolor sit amet consectetur.</li>
+              <li>Lorem ipsum dolor sit amet consectetur.</li>
+            </ul>
+            <button>Aplica aici</button>
+          </div>
+        </div>
+        <div className="h-auto lg:h-28 bg-green-00 text-center my-[20px] hidden lg:block">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+            nobis labore, fugiat a excepturi, cum ducimus eos laborum ut quos
+            facere possimus perspiciatis odit, magnam laboriosam velit beatae
+            architecto neque?
+          </p>
+        </div>
       </div>
     </section>
   );
